58f38c38db72a76846e33b2e6d0c0b3c43aa0645
Laurent Peuch <cortex@worlddomination.be>
2014-11-14 07:29:00 +0100

    [enh] pass test with dot

--- pyfmt.py
+++ pyfmt.py
@@ -729,6 +729,10 @@ empty_string = lambda _: ""
 
 
 custom_key_renderers = {
+    "dot": {
+        "first_formatting": empty_string,
+        "second_formatting": empty_string,
+    },
     "ellipsis": {
         "first_formatting": empty_string,
         "second_formatting": empty_string,
